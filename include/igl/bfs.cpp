fa4f1a925e2df366a160f7641f0f510a02c0ac7c
Hank Shen <sunshen.hx@gmail.com>
2019-10-30 16:06:52 -0400

    drop __1

--- include/igl/bfs.cpp
+++ include/igl/bfs.cpp
@@ -91,4 +91,4 @@ IGL_INLINE void igl::bfs(
 
 }
 template void igl::bfs<std::vector<std::vector<int, std::allocator<int> >, std::allocator<std::vector<int, std::allocator<int> > > >, Eigen::Matrix<int, -1, 1, 0, -1, 1>, Eigen::Matrix<int, -1, 1, 0, -1, 1> >(std::vector<std::vector<int, std::allocator<int> >, std::allocator<std::vector<int, std::allocator<int> > > > const&, unsigned long, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, 1, 0, -1, 1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, 1, 0, -1, 1> >&);
-template void igl::bfs<Eigen::SparseMatrix<bool, 0, int>, int, int>(Eigen::SparseCompressedBase<Eigen::SparseMatrix<bool, 0, int> > const&, unsigned long, std::__1::vector<int, std::__1::allocator<int> >&, std::__1::vector<int, std::__1::allocator<int> >&);
+template void igl::bfs<Eigen::SparseMatrix<bool, 0, int>, int, int>(Eigen::SparseCompressedBase<Eigen::SparseMatrix<bool, 0, int> > const&, unsigned long, std::vector<int, std::allocator<int> >&, std::vector<int, std::allocator<int> >&);
